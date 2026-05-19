f1ec770a044cf87491ad2615135902729c52b6e4
Philipp Mukosey <philipp@mukosey.com>
2026-05-19 22:12:06 +0200

    Enable Tailscale

--- configuration.nix
+++ configuration.nix
@@ -970,7 +970,22 @@ in
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  networking.firewall.enable = true;
+  services.tailscale.enable = true;
+  networking.nftables.enable = true;
+  networking.firewall = {
+    enable = true;
+    # Always allow traffic from your Tailscale network
+    trustedInterfaces = [ "tailscale0" ];
+    # Allow the Tailscale UDP port through the firewall
+    allowedUDPPorts = [ config.services.tailscale.port ];
+  };
+
+  # 2. Force tailscaled to use nftables (Critical for clean nftables-only systems)
+  # This avoids the "iptables-compat" translation layer issues.
+  systemd.services.tailscaled.serviceConfig.Environment = [ 
+    "TS_DEBUG_FIREWALL_MODE=nftables" 
+  ];
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = false; # only enable manually when needed to save power & for security
