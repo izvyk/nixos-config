79593f3cc13b51f6d3384ec06024978fe67e8d99
Philipp Mukosey <philipp@mukosey.com>
2026-08-20 03:15:29 +0200

    Extract zram to a separate module

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,7 @@ in
 
   imports = [
     ./hardware-configuration.nix
+    ./zram.nix
     ./gnome.nix
     ./dock-mode.nix
     ./input.nix
@@ -633,30 +634,6 @@ in
     '')
   ];
 
-  zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 50;
-    priority = 100; # Maximum priority. Kernel uses this first.
-  };
-
-  # Dynamically allocate the physical swapfile ONLY during hibernation lifecycles.
-  # The swapfile is completely invisible to the kernel during normal runtime.
-  systemd.services.systemd-hibernate.serviceConfig = {
-    ExecStartPre = "${pkgs.util-linux}/bin/swapon /swap/swapfile";
-    ExecStopPost = "${pkgs.util-linux}/bin/swapoff /swap/swapfile";
-  };
-
-  systemd.services.systemd-suspend-then-hibernate.serviceConfig = {
-    ExecStartPre = "${pkgs.util-linux}/bin/swapon /swap/swapfile";
-    ExecStopPost = "${pkgs.util-linux}/bin/swapoff /swap/swapfile";
-  };
-
-  systemd.services.systemd-logind = {
-    environment = {
-      SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK = "1";
-    };
-  };
   services.logind.settings.Login = {
     HandleLidSwitch = "suspend-then-hibernate";
     # lidSwitchDocked = "ignore";
